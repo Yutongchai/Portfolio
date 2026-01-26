2012d7811601870095c86963973cdd7acd9e68bf
Wong Qiao Ying <129234636+qiaoying1014@users.noreply.github.com>
2026-01-26 10:59:02 +0800

    Remove scroll to top button

--- src/components/ui/ScrollToTopButton.tsx
+++ src/components/ui/ScrollToTopButton.tsx
@@ -25,28 +25,6 @@ const ScrollToTopButton = () => {
       behavior: 'smooth'
     });
   };
-
-  return (
-    <button
-      onClick={scrollToTop}
-      className={`scroll-to-top-button ${isVisible ? 'visible' : ''}`}
-      aria-label="Scroll to top"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <polyline points="18 15 12 9 6 15" />
-      </svg>
-    </button>
-  );
 };
 
 export default ScrollToTopButton;
