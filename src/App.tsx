0b2bae76d9ed18e5a2088a8c41b3799324293417
Yutong.C <yutongchai2@gmail.com>
2026-03-02 23:05:56 +0800

    added new google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routes from "./Routes";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import ScrollToTopButton from "./components/ui/ScrollToTopButton";
 import WhatsAppFloating from "./components/ui/WhatsAppFloating";
 import CookieConsent from "./components/CookieConsent";
-
+import ReactGA from 'react-ga4';
+   
 const App: React.FC = () => {
+  useEffect(() => {
+       ReactGA.send({ hitType: "pageview", page: location.pathname });
+     }, [location]);
   return (
     <ThemeProvider>
       <AuthProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
+    
     <App />
   </StrictMode>,
+    ReactGA.initialize("G-J45HZDCVNQ")
 )
